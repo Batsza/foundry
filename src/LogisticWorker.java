00d16318f6daba62bc20c984ffd590c1fe14a0d8
Batsza <konrad_szabat@wp.pl>
2020-12-13 17:26:25 +0100

    updatelogistic

--- src/LogisticWorker.java
+++ src/LogisticWorker.java
@@ -5,6 +5,32 @@ public class LogisticWorker extends Worker {
     Material[] material = new Material[100];
     Scanner scan = new Scanner(System.in);
 
+    int numberOfWarehouse = 0;
+    Warehouse[] warehouse = new Warehouse[5];
+    public void  addNewWarehouse(){
+        if(numberOfWarehouse == 0){
+            for(int i = numberOfWarehouse; i < warehouse.length; i++) {
+                warehouse[i] = new Warehouse();
+            }
+        }
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Podaj id magzynu: ");
+        Integer a = scan.nextInt();
+        warehouse[numberOfWarehouse].setIdWarehouse(a);
+        System.out.print("Podaj pojemnosc: ");
+        Integer b = scan.nextInt();
+        warehouse[numberOfWarehouse].setCapacity(b);
+
+        numberOfWarehouse++;
+    }
+    public void showWarehouseList(){
+        for(int i = 1; i < numberOfWarehouse+1; i++) {
+            System.out.println("Magazyn ");
+            warehouse[i-1].warehouseList();
+        }
+    }
+
+
     public void  addNewMaterial(){
         if(numberOfMaterials == 0){
             for(int i = numberOfMaterials; i < material.length; i++) {
